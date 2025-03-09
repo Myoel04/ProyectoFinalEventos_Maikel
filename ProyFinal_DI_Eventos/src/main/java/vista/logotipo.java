ec239275b966a31cee5ffbe3f092d214b937d0f3
Maikel Yoel Romero Loayza <127664171+Myoel04@users.noreply.github.com>
2025-03-09 12:01:05 +0100

    Movimiento Logotipo

--- ProyFinal_DI_Eventos/src/main/java/vista/logotipo.java
+++ ProyFinal_DI_Eventos/src/main/java/vista/logotipo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ventanas;
+package vista;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -13,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JWindow;
 import javax.swing.SwingConstants;
+import ventanas.ventLogin;
 
 /**
  *
